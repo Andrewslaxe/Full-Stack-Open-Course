8a42d9c37f55ef148e955ddee8a9b44eba660a35
Kengan <jaimeperaza2001@gmail.com>
2023-04-16 15:18:40 -0500

    Updated db

--- Episode3/phonebook-backend/services/info.js
+++ Episode3/phonebook-backend/services/info.js
@@ -1,7 +1,7 @@
-import { persons } from './persons.js'
+import persons from './persons.js'
 
 export const info = () => {
-  const infoString = `Phonebook has info for ${persons.length} people <br/><br/>`
+  const infoString = `Phonebook has info for ${persons.persons} people <br/><br/>`
   const actualTime = new Date()
   return infoString + actualTime
 }
