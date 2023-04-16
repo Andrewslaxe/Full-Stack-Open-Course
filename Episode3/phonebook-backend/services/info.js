34f53c7269f206aee84daacc462c2e61cdfc9a2e
Kengan <jaimeperaza2001@gmail.com>
2023-04-16 15:29:24 -0500

    fixed info

--- Episode3/phonebook-backend/services/info.js
+++ Episode3/phonebook-backend/services/info.js
@@ -1,7 +1,7 @@
 import persons from './persons.js'
 
 export const info = () => {
-  const infoString = `Phonebook has info for ${persons.persons} people <br/><br/>`
+  const infoString = `Phonebook has info for ${persons.persons.length} people <br/><br/>`
   const actualTime = new Date()
   return infoString + actualTime
 }
